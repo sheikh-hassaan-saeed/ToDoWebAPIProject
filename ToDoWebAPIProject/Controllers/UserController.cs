d7e4d23b06f9568cd84b8d0c4a20f6d64734d277
Sheikh Hassaan Saeed <sheikhhassaansaeed@gmail.com>
2026-02-26 12:28:35 +0500

    get user by id api

--- ToDoWebAPIProject/Controllers/UserController.cs
+++ ToDoWebAPIProject/Controllers/UserController.cs
@@ -11,6 +11,53 @@ namespace ToDoWebAPIProject.Controllers
     {
         public readonly UserContext _userContext;
 
-        public 
+        public UserController(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+
+        [HttpGet]
+        [Route("GetAllUsers")]
+        public async Task<ActionResult<IEnumerable<User>>> GetAllUser()
+        {
+            var users = await _userContext.users.ToListAsync();
+            if (users.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(users);
+        }
+        [HttpPost]
+        [Route("AddUser")]
+        public async Task<ActionResult<User>> AddUser(User user)
+        {
+            _userContext.users.Add(user);
+            await _userContext.SaveChangesAsync();
+            return Ok(User);
+        }
+
+
+        [HttpPut]
+        [Route("UpdateUser")]
+        public async Task<ActionResult<IEnumerable<User>>> UpdateUser(User user)
+        {
+            _userContext.users.Update(user);
+            await _userContext.SaveChangesAsync();
+
+            return Ok(string.Empty);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUserByID(int id)
+        {
+            var user =  await _userContext.users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+
+        }
+
     }
 }
